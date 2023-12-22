2effacbe091fd94f7e4f4cd1f27c49f5c52dd1a4
tiwentichat <125454744+tiwentichat@users.noreply.github.com>
2023-12-22 14:06:39 +0800

    Update main.jsx 测试：添加 register

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./Global.style";
 
+import register from "./registerServiceWorker";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
