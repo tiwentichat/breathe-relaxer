0995432d1a5baebbf5b2cfa9140b1e888b9f7146
tiwentichat <125454744+tiwentichat@users.noreply.github.com>
2023-12-22 14:12:33 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./Global.style";
 
-import register from "./registerServiceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
